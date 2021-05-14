f0b0329331b3ec518ad9ce4e136c0e57eab2466d
Prajesh Ananthan <prajesh.ananthan@airasiabig.com>
2021-05-14 18:17:56 +0800

    List events

--- pages/events/index.js
+++ pages/events/index.js
@@ -1,7 +1,12 @@
+import {getAllEvents} from "../../dummy-data";
+import EventList from "../../components/events/event-list";
+
 function EventsPage() {
+
+  const events = getAllEvents();
   return (
     <div>
-      <h1>The Event Page</h1>
+      <EventList items={events}/>
     </div>
   )
 }
