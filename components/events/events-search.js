46663e59bf9dd3ebbc23846228b1cdbebf9e8670
Prajesh Ananthan <prajesh.ananthan@airasiabig.com>
2021-05-14 23:51:28 +0800

    Navigate to the filter event page programmatically

--- components/events/events-search.js
+++ components/events/events-search.js
@@ -1,25 +1,44 @@
 import Button from "../ui/button";
 import classes from './event-search.module.css';
+import {useRef} from "react";
 
 function EventSearch(props) {
+
+  const yearInputRef = useRef();
+  const monthInputRef = useRef();
+
+  // We get the event object by default DOM event
+  function submitHandler(event) {
+    // Prevent the browser from sending HTTP request, reloading
+    // the browser and losing the application state
+    event.preventDefault();
+    const selectedYear = yearInputRef.current.value;
+    const selectedMonth = monthInputRef.current.value;
+
+    console.log(selectedYear);
+    console.log(selectedMonth);
+
+    props.onSearch(selectedYear, selectedMonth);
+  }
+
   return (
-    <form className={classes.form}>
+    <form className={classes.form} onSubmit={submitHandler}>
       <div className={classes.controls}>
         <div className={classes.control}>
           <label htmlFor='year'>Year</label>
-          <select id="year">
+          <select id="year" ref={yearInputRef}>
             <option value='2021'>2021</option>
             <option value='2022'>2022</option>
           </select>
         </div>
         <div className={classes.control}>
           <label htmlFor="month">Month</label>
-          <select id="month">
+          <select id="month" ref={monthInputRef}>
             <option value='1'>January</option>
             <option value='2'>February</option>
             <option value='3'>March</option>
             <option value='4'>April</option>
-            <option value='5'>Nay</option>
+            <option value='5'>May</option>
             <option value='6'>June</option>
             <option value='7'>July</option>
             <option value='7'>July</option>
